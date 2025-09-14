06aa0bedb7d5ad43075da94afaeab75bbf03d7ff
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-09-14 14:10:12 +0530

    Positive negative or zero checking

---
+++ Beginner/src/PositieveOrNegaticeOrZeroChecking.java
@@ -0,0 +1,9 @@
+
+public class PositieveOrNegaticeOrZeroChecking {
+    public static void main(String[] args){
+        int a = new java.util.Scanner(System.in).nextInt();
+        if(a>0) System.out.println("Positive");
+        else if(a<0) System.out.println("Negative");
+        else System.out.println("Zero");
+    }
+}
