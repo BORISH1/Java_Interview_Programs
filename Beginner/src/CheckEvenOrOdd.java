eb2eef52e6cedd93de50008949526af63f2ed8a7
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-09-14 13:51:18 +0530

    Check Even or Odd

---
+++ Beginner/src/CheckEvenOrOdd.java
@@ -0,0 +1,6 @@
+public class CheckEvenOrOdd {
+    public static void main(String[] args){
+        int n = new java.util.Scanner(System.in).nextInt();
+        System.out.println(n % 2 ==0 ? "Even" : "Odd");
+    }
+}
