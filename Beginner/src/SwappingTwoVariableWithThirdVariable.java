01c4e2515d9486b97932374c2123299bf13c56c0
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-09-14 14:29:11 +0530

    Swapping two variable with a third variable

---
+++ Beginner/src/SwappingTwoVariableWithThirdVariable.java
@@ -0,0 +1,9 @@
+public class SwappingTwoVariableWithThirdVariable {
+    public static void main(String[] args){
+        int a = 10 , b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+}
