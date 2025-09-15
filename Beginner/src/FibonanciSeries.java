699819ae32fbb3a6eabbd2e7bfcec7481decc2df
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-09-15 23:42:49 +0530

    FibonanciSeries

---
+++ Beginner/src/FibonanciSeries.java
@@ -0,0 +1,10 @@
+public class FibonanciSeries {
+    public static void main(String[] args) {
+        int n = new java.util.Scanner(System.in).nextInt();
+        long a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.println(a + (i<n-1?" ":" "));
+            long c = a+b; a=b ; b=c;
+        }
+    }
+}
